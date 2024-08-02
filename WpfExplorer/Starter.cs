d72e021905f31e5a2f7dd6138411941ab5bf5296
보성 이 <lbs6603@udmtek.com>
2024-08-02 16:03:50 +0900

    초기 커밋.

---
+++ WpfExplorer/Starter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfExplorer
+{
+    internal class Starter
+    {
+        private static void Main(string[] args)
+        {
+            _ = new App().Run();
+        }
+    }
+}
